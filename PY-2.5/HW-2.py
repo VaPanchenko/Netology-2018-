7f8bea6dacd18c07f38883615e6c85a053bdc104
Vyacheslav <panchenko.slava@gmail.com>
2018-06-13 17:00:46 +0700

    PY-2.5

--- PY-2.5/HW-2.py
+++ PY-2.5/HW-2.py
@@ -17,23 +17,30 @@ def get_file_list(files_path):
     return files
 
 
-def convert_jpg(files, files_path_result, files_path_sourse):
+def convert_jpg(file):
     import subprocess
     import os
 
-    for file in files:
-        subprocess.Popen(['sips', '--resampleWidth', '200', '--out', os.path.join(files_path_result, file), os.path.join(files_path_sourse, file)])
+    files_path_sourse, files_path_result = get_path_files()
+
+    subprocess.run(['sips', '--resampleWidth', '200', '--out', os.path.join(files_path_result, file), os.path.join(files_path_sourse, file)])
 
 
 def main():
     import os
+    from multiprocessing import Pool
+
     files_path_sourse, files_path_result = get_path_files()
     files = get_file_list(files_path_sourse)
+
     if os.path.exists(files_path_result):
         pass
     else:
         os.mkdir(files_path_result)
-    convert_jpg(files, files_path_result, files_path_sourse)
+
+    if __name__ == '__main__':
+            pool = Pool(processes=4)
+            pool.map(convert_jpg, files)
 
 
 main()
