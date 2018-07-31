ad533f0da49ae16fac741311dd8c766923efcd06
Vyacheslav <panchenko.slava@gmail.com>
2018-07-31 14:30:10 +0700

    Финальная работа

--- Финальная работа/FW.py
+++ Финальная работа/FW.py
@@ -3,9 +3,14 @@ import progressbar
 import time
 import json
 
-user = 'tim_leary'
-v = '5.78'
-TOKEN = '7b23e40ad10e08d3b7a8ec0956f2c57910c455e886b480b7d9fb59859870658c4a0b8fdc4dd494db19099'
+
+
+with open('user.json', 'r', encoding='utf8') as file:
+    data = json.load(file)
+
+user = data['user']
+v = data['v']
+TOKEN = data['TOKEN']
 
 
 def get_user_id():
@@ -17,6 +22,7 @@ def get_user_id():
         'v': v,
         'user_ids': user,
     }
+
     user_id = requests.get('https://api.vk.com/method/users.get',
                            params=params).json()['response'][0]['id']
 
@@ -47,8 +53,12 @@ def get_groups_target_user(user_id):
         'v': v,
         'user_id': user_id,
     }
-    groups = requests.get('https://api.vk.com/method/groups.get',
+
+    while True:
+        groups = requests.get('https://api.vk.com/method/groups.get',
                           params=params).json()['response']['items']
+        if groups:
+            break
 
     return groups
 
@@ -66,50 +76,87 @@ def get_groups(friends):
                 'v': v,
                 'user_id': friend,
             }
+            while True:
 
-            groups.update({friend: requests.get('https://api.vk.com/method/groups.get',
+                groups.update({friend: requests.get('https://api.vk.com/method/groups.get',
                                                 params=params).json()['response']['items']})
+                if groups:
+                    break
 
             time.sleep(0.34)
             bar.update(i)
-        except:
+
+        except (KeyError, ValueError):
             continue
 
     return groups
 
 
+
+
 def get_user_groups_info(union_user_groups):
 
     groups_user_list_info = []
-
     print('\nПолучение данных об уникальных группах пользователя')
-    bar = progressbar.ProgressBar(max_value=len(union_user_groups))
 
-    for i, group in enumerate(union_user_groups):
-        try:
-            info_list = dict()
+    group_list = [union_user_groups[i:i + 10] for i in range(0, len(union_user_groups), 10)]
 
-            params = {
-                'access_token': TOKEN,
-                'v': v,
-                'group_id': group,
-                'fields': 'members_count'
-            }
+    print(type(group_list))
+    print(group_list[0])
 
-            group_info = requests.get('https://api.vk.com/method/groups.getById',
-                                      params=params).json()['response'][0]
 
-            info_list['gid'] = group_info['id']
-            info_list['name'] = group_info['name']
-            info_list['members_count'] = group_info['members_count']
+    for i, group in enumerate(group_list):
+        info_list = dict()
 
-            groups_user_list_info.append(info_list)
+        params = {
+            'access_token': TOKEN,
+            'v': v,
+            'group_ids': group,
+            'fields': 'members_count'
+        }
 
-            # time.sleep(0.34)
-            bar.update(i)
 
-        except:
-            continue
+        group_info = requests.get('https://api.vk.com/method/groups.getById',
+                                        params=params).json()['response'][0]
+
+
+        print(type(group_info))
+        print(group_info)
+
+        groups_user_list_info.append(group_info)
+        time.sleep(0.33)
+        # print(groups_user_list_info)
+
+
+    # bar = progressbar.ProgressBar(max_value=len(union_user_groups))
+
+    # for i, group in enumerate(union_user_groups):
+    #     try:
+    #         info_list = dict()
+    #
+            # params = {
+            #     'access_token': TOKEN,
+            #     'v': v,
+            #     'group_id': group,
+            #     'fields': 'members_count'
+            # }
+    #         while True:
+    #
+                # group_info = requests.get('https://api.vk.com/method/groups.getById',
+                #                       params=params).json()['response'][0]
+    #             if group_info:
+    #                 break
+    #
+    #         info_list['gid'] = group_info['id']
+    #         info_list['name'] = group_info['name']
+    #         info_list['members_count'] = group_info['members_count']
+    #
+    #         groups_user_list_info.append(info_list)
+    #
+    #         bar.update(i)
+    #
+    #     except (KeyError, ValueError):
+    #         continue
 
     return groups_user_list_info
 
@@ -120,18 +167,27 @@ def main():
     friends = get_friends(user_id)
     user_groups = set(get_groups_target_user(user_id))
     user_friends_group = get_groups(friends)
-    super_group = set()
+    union_friends_groups = set()
 
     for i in user_friends_group:
-        super_group = super_group.union(set(user_friends_group.get(i)))
+        union_friends_groups = union_friends_groups.union(set(user_friends_group.get(i)))
 
-    union_user_groups = list(user_groups.difference(super_group))
+    union_user_groups = list(user_groups.difference(union_friends_groups))
 
     result = get_user_groups_info(union_user_groups)
-    print(result)
 
     with open('groups.json', 'w', encoding='utf8') as file:
         json.dump(result, file,  ensure_ascii=False)
 
 
-main()
+# main()
+
+user_id = get_user_id()
+
+group = get_groups_target_user(user_id)
+
+
+print('___________________________')
+print(get_user_groups_info(group))
+
+
